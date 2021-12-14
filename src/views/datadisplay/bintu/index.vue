d0126bb23b3f69b384b5cad46824921cfec72994
Fe2O3-amber <2644822514@qq.com>
2021-12-15 00:23:24 +0800

    完善了部分饼图内容

---
+++ src/views/datadisplay/bintu/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="pie">
+        <div id="pie1">
+            <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+            <div id="main1" style="float:left;width:100%;height: 300px"></div>
+        </div>
+        <div id="pie2">
+            <div id="main2" style="float:left;width:100%;height: 300px"></div>
+        </div>
+    </div>
+  </div>
+</template>
+ 
+<script>// 引入基本模板
+let echarts = require('echarts/lib/echarts')
+// 引入饼状图组件
+require('echarts/lib/chart/pie')
+// 引入提示框和title组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
+ 
+export default {
+  created(){
+  },
+  mounted(){
+    this.initData();
+  },
+  methods:{
+    //初始化数据
+    initData() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById('main1'));
+      // 绘制图表
+      myChart.setOption({
+          title : {
+              text: '公司花销分布',//主标题
+              x:'center',//x轴方向对齐方式
+          },
+          tooltip : {
+              trigger: 'item',
+              formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          legend: {
+              orient: 'vertical',
+              bottom: 'bottom',
+              data: ['工资支出','卖票收入','水电费']
+          },
+          series : [
+              {
+                  name: '支出:',
+                  type: 'pie',
+                  radius : '55%',
+                  center: ['50%', '60%'],
+                  data:[
+                      {value:335, name:'工资支出'},
+                      {value:310, name:'卖票收入'},
+                      {value:234, name:'水电费'}
+                  ],
+                  itemStyle: {
+                      emphasis: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                  }
+              }
+          ]
+      });
+    },
+  }
+}
+</script>
