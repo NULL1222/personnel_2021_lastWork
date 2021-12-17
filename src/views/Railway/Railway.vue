7164cdd7f6f9f993442a471ab86c8371a2fbe9bb
LIGHTROMANCE <RachelW9120@gmail.com>
2021-12-17 17:35:24 +0800

    Railway表格修改+数据连接

--- src/views/Railway/Railway.vue
+++ src/views/Railway/Railway.vue
@@ -1,55 +1,71 @@
 <template>
   <div>
-    <!-- <div class="head"> -->
-    <el-form :model="formInline" class="demo-form-inline">
-      <!-- <el-form :inline="true" :model="formInline" class="demo-form-inline"> -->
-      <el-form-item label="车次">
-        <el-input v-model="formInline.id" placeholder="G8888" prefix-icon="el-icon-search" style="width: 100px;"></el-input>
-      </el-form-item>
-      <el-form-item label="出发地">
-        <el-input v-model="formInline.depStation" placeholder="上海" prefix-icon="el-icon-search" style="width: 100px;"></el-input>
-      </el-form-item>
-      <el-form-item label="目的地">
-        <el-input v-model="formInline.desStation" placeholder="宁波" prefix-icon="el-icon-search" style="width: 100px;"></el-input>
-      </el-form-item>
-      <!-- <div class="headTime"> -->
-      <el-form-item label="出发日">
-        <el-date-picker type="date" placeholder="选择日期" v-model="formInline.date1" style="width: 80%;"></el-date-picker>
-      </el-form-item>
-      <el-form-item label="起始时间">
-        <el-time-picker
-          v-model="formInline.value1"
-          placeholder="选择时间"
-          value-format="HH:mm"
-          format="HH:mm">
-        </el-time-picker>
-      </el-form-item>
-      <el-form-item label="返程日">
-        <el-date-picker type="date" placeholder="选择日期" v-model="formInline.date2" style="width: 80%;"></el-date-picker>
-      </el-form-item>
-      <el-form-item label="结束时间">
-        <el-time-picker
-          v-model="formInline.value2"
-          placeholder="任意时间点"
-          value-format="HH:mm"
-          format="HH:mm">
-        </el-time-picker>
-      </el-form-item>
-      <!-- </div> -->
-      <el-form-item>
+    <el-form label-position="labelPosition" label-width="115px" :model="formInline" class="demo-form-inline">
+      <div class="form-left">
+        <el-form-item label="车次">
+          <el-input v-model="formInline.id" placeholder="G8888" prefix-icon="el-icon-search"></el-input>
+        </el-form-item>
+        <el-form-item label="出发地">
+          <el-input v-model="formInline.depStation" placeholder="上海" prefix-icon="el-icon-search" style="width: 100%;"></el-input>
+        </el-form-item>
+        <el-form-item label="目的地">
+          <el-input v-model="formInline.desStation" placeholder="宁波" prefix-icon="el-icon-search" style="width: 100%;"></el-input>
+        </el-form-item>
+        <div style="width: 100%;"></div>
+        <el-form-item label="选择日期、时间">
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="起始日期" v-model="formInline.startDay" style="width: 100%;"></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker 
+              placeholder="选择时间" 
+              v-model="formInline.value1" 
+              style="width: 100%;"
+              format="HH:mm">
+            </el-time-picker>
+          </el-col>
+        </el-form-item>
+          <el-form-item label-width="10px">
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="结束日期" v-model="formInline.endDay" style="width: 100%;"></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker 
+              placeholder="选择时间" 
+              v-model="formInline.value2" 
+              style="width: 100%;"
+              format="HH:mm">
+            </el-time-picker>
+          </el-col>
+        </el-form-item>
+      </div>
+      <div class="form-right">
         <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
+        <el-button type="primary" @click="refreshing">刷新</el-button>
+        <el-button type="primary" plain @click="resetForm">重置</el-button>
+      </div>
+
     </el-form>
-  <!-- </div> -->
     <el-table
     :data="rolesList"
     stripe
     :header-cell-style="{background: 'lightblue', color: '#fff'}"
     style="width: 90%; margin-top: 30px;margin-left: 30px;">
     <el-table-column
+      label="日期"
+      fixed
+      width="100"
+      align="center">
+      <template slot-scope="scope">
+        {{ scope.row.date }}
+      </template>
+    </el-table-column>
+    <el-table-column
       fixed
       label="车次"
-      width="120"
+      width="100"
       align="center">
       <template slot-scope="scope">
         {{ scope.row.id }}
@@ -160,104 +176,248 @@
   export default {
     data() {
       return {
-        rolesList: [{
-          id: 'G4535',
-          depStation: '上海',
-          desStation: '宁波',
-          depTime: '7:00',
-          desTime: '8:30',
-          lastTime: '1小时30分',
-          firstClass: '23',
-          firstPrice: '100元',
-          secondClass: '55',
-          secondPrice: '80元',
-          noSeats: '售空',
-          thirdPrice: "50元"
-        }, 
-        {
-          id: 'D553',
-          depStation: '杭州',
-          desStation: '北京',
-          depTime: '7:00',
-          desTime: '13:30',
-          lastTime: '6小时30分',
-          firstClass: '66',
-          firstPrice: '100元',
-          secondClass: '189',
-          secondPrice: '80元',
-          noSeats: '45',
-          thirdPrice: "50元"
-        },
-        {
-          id: 'D553',
-          depStation: '杭州',
-          desStation: '北京',
-          depTime: '7:00',
-          desTime: '13:30',
-          lastTime: '6小时30分',
-          firstClass: '66',
-          firstPrice: '100元',
-          secondClass: '189',
-          secondPrice: '80元',
-          noSeats: '45',
-          thirdPrice: "50元"
-        },
-        {
-          id: 'D553',
-          depStation: '杭州',
-          desStation: '北京',
-          depTime: '7:00',
-          desTime: '13:30',
-          lastTime: '6小时30分',
-          firstClass: '66',
-          firstPrice: '100元',
-          secondClass: '189',
-          secondPrice: '80元',
-          noSeats: '45',
-          thirdPrice: "50元"
-        },
+        rolesList: [
+          // {
+        //   date: '12.1',
+        //   id: 'G4535',
+        //   depStation: '上海',
+        //   desStation: '宁波',
+        //   depTime: '7:00',
+        //   desTime: '8:30',
+        //   lastTime: '1小时30分',
+        //   firstClass: '23',
+        //   firstPrice: '100元',
+        //   secondClass: '55',
+        //   secondPrice: '80元',
+        //   noSeats: '售空',
+        //   thirdPrice: "50元"
+        // }, 
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
+        // {
+        //   date: '12.1',
+        //   id: 'D553',
+        //   depStation: '杭州',
+        //   desStation: '北京',
+        //   depTime: '7:00',
+        //   desTime: '13:30',
+        //   lastTime: '6小时30分',
+        //   firstClass: '66',
+        //   firstPrice: '100元',
+        //   secondClass: '189',
+        //   secondPrice: '80元',
+        //   noSeats: '45',
+        //   thirdPrice: "50元"
+        // },
         ],
         formInline: {
           id: '',
           depStation: '',
           desStation: '',
-          date1: '',
-          date2: '',
-          value1: new Date(),
-          value2: new Date()
-        } 
+          startDay: '',
+          endDay: '',
+          value1: '',
+          value2: ''
+        },
+        labelPosition: 'right',
+        // pickerOptions0: {
+        //   disabledDate(time) {
+        //     return time.getTime() < Date.now() - 8.64e7;
+        //   }
+        // } 
       }
     },
+    created() {
+      this.setTime()
+    },
+    mounted: function() {
+        this.initUser()
+      },
     methods: {
+      setTime(){
+        const nowD = new Date()
+        nowD.setTime(nowD.getTime())
+        this.formInline.startDay = nowD
+        this.formInline.endDay = nowD
+        this.formInline.value1 = nowD
+        this.formInline.value2 = nowD
+      },
+      initUser() {
+        var _this = this
+          this.$axios.post("/train/initial?startDay="+this.formInline.startDay+"&endDay="+this.formInline.endDay, {}).then(resp => {
+            if (resp && resp.data.code === 200) {
+              _this.rolesList = resp.data.data
+            }
+          })
+      },
       handleClick(id) {
         console.log(id);
       },
       onSubmit() {
         console.log('submit!');
-      }
+      },
+      refreshing() {
+        location.reload()
+      },
+      resetForm() {
+        this.formInline.id = "",
+        this.formInline.depStation = "",
+        this.formInline.desStation = "",
+        this.$options.methods.setTime.bind(this)()
+      },
     },
   }
 </script>
 
 <style scoped>
 
-.head {
+.demo-form-inline {
   margin-top: 30px;
   display: flex;
-  justify-content: left;
-  align-items: center; 
+  justify-content: flex-start;
+  align-items: flex-start; 
+  flex-wrap: wrap;
+  flex-direction: row;
 }
 
-.headTime {
+.form-left {
+  flex: 1;
+  flex-direction: row;
   display: flex;
-  justify-content: left;
-  /* align-items: left;  */
+  justify-content: flex-start;
+  align-items: flex-start; 
+  flex-wrap: wrap;
 }
 
-/* .demo-form-inline {
+.form-right {
+  width: 200px;
   display: flex;
-  justify-content: center;
-  align-items: center; 
-} */
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+}
+
+.form-right button {
+  margin-left: 0;
+  margin-bottom: 20px;
+}
 
 </style>
